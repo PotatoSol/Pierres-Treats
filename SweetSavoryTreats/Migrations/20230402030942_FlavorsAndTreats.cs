4d80a881537b3c6244d416a8dcc0c7c052ddc0ba
Michael Sol <sol.mike.j@gmail.com>
2023-04-01 20:13:25 -0700

    add new migration

---
+++ SweetSavoryTreats/Migrations/20230402030942_FlavorsAndTreats.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SweetSavoryTreats.Migrations
+{
+    public partial class FlavorsAndTreats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
